7eedad98ed21efcfc16bb84f4a47b4c756ad4f9c
strix <damjan.oslaj@gmail.com>
2019-09-09 08:50:19 +0200

    changed user auth

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -93,6 +93,30 @@ router.get('/login', function(req, res) {
   res.render('login', {page:'login', menuId:'login', session: req.session});
 });
 
+//FIXME:
+//POST route for updating data
+router.post('/login', function (req, res, next) {
+  
+  if (req.body.logemail && req.body.logpassword) {
+    //login
+    User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/dashboard');
+      }
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+});
+//FIXME:
+
 
 //POST route for updating data
 router.post('/register', function (req, res, next) {
